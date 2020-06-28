944ae88531193e38d3869292d43a865df7409354
bfsgr <bfsgr.code@protonmail.com>
2020-06-27 22:09:50 -0300

    Validations to character

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,9 +3,16 @@ class Character < ApplicationRecord
     has_many :stats
     has_one :current_stat
 
-    def self.professions
-        [
-            ['Advogado', ],
-        ]
+    validates :name, :presence => true, :length => { :in => 3..50 }
+    validates :ocupation, :presence => true
+    validates :scholarity, :presence => true
+    validates :birth_place, :presence => true
+    validates :age, :presence => true
+    validates :sex, :presence => true
+    
+    validates_length_of :password, :in => 6..255, :on => :create
+
+    def self.ocupations 
+        
     end
 end
