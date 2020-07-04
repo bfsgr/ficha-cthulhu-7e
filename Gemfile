14a14383ffec8dd629f48958cc039ee2f3c3ea75
bfsgr <bfsgr.code@protonmail.com>
2020-07-04 02:58:14 -0300

    fix fontawesome in production

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3', '~> 1.4'
 gem 'pg', '~> 1.2.3'
+
+gem 'font-awesome-rails', '>= 4.7.0.5'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,8 @@ require('jquery')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all";
 import 'bootstrap'
 
 document.addEventListener("turbolinks:load", () => {
