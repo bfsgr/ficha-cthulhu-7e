916703939e733f097b7a92635245d27e766d4a64
bfsgr <bfsgr.code@protonmail.com>
2020-07-03 23:48:38 -0300

    Some work on stat model

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,5 +7,13 @@ class StatsController < ApplicationController
     end
 
     def create
+        @stats = Stat.new(stat_params.merge(:character_id => @char.id))
+        @stats.save
+        render plain: @stats.inspect
+    end
+
+    private
+    def stat_params
+        params.require(:stat).permit(:str, :dex, :app, :int, :pow, :edu, :siz, :con)
     end
 end
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,3 +1,36 @@
 class Stat < ApplicationRecord
     belongs_to :character
+    before_save :derive
+
+
+    validates :str, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+    validates :dex, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+    validates :int, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+    validates :app, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+    validates :pow, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+    validates :edu, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+    validates :con, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+    validates :siz, :presence => true,  numericality: { only_integer: true, less_than: 91, greater_than: 14 }
+
+    validate :sum_less_than_461
+
+    private
+    def sum_less_than_461
+        if (str.to_i + dex.to_i + int.to_i + app.to_i + pow.to_i + edu.to_i + con.to_i + siz.to_i) >= 460
+            errors.add(:base, "Pontos maiores do que 460") 
+        end
+    end
+
+    def derive
+        self.lvl = 1
+        self.luc = rand(15..90)
+
+        if str < siz and dex < siz 
+            self.mov = 7
+        elsif str > siz and dex > siz
+            self.mov = 9
+        else
+            self.mov = 8
+        end
+    end
 end
