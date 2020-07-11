b77e16d1f7845ef90d82df8c78050cba0628c1b8
bfsgr <bfsgr.code@protonmail.com>
2020-07-10 21:14:57 -0300

    Enforce age limits

--- app/models/character.rb
+++ app/models/character.rb
@@ -39,7 +39,7 @@ class Character < ApplicationRecord
     validates :name, :presence => true, :length => { :in => 3..50 }
     validates :ocupation, :presence => true, numericality: { only_integer: true, less_than: 28, greater_than: -1 }
     validates :birth_place, :presence => true
-    validates :age, :presence => true
+    validates :age, :presence => true, numericality: { only_integer: true, greater_than: 17, less_than: 90}
     validates :sex, :presence => true
     
 
