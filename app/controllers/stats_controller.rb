2c5881e4d96b435cea95c6c6ff20bf8afa7beb47
bfsgr <bfsgr.code@protonmail.com>
2020-07-12 15:55:48 -0300

    Some work

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -10,8 +10,42 @@ class StatsController < ApplicationController
     end
     
     def age_limits
-        if stats_set() # => generate @stat
-            
+        if stats_set() # => generate @stat            
+            case @char.age
+            when 18..19
+                @change = [:str, :siz]
+                @must = :edu
+                @points = { change: 5, must: 5 }
+                @details = { change: ['Força', 'Tamanho'], must: 'Educação' }
+            when 20..39
+            when 40..49
+                @change = [:str, :con, :dex]
+                @must = :app
+                @points = { change: 5, must: 5 }
+                @details = { change: ['Força', 'Constituição', 'Destreza'], must: 'Aparência' }
+            when 50..59
+                @change = [:str, :con, :dex]
+                @must = :app
+                @points = { change: 10, must: 10 }
+                @details = { change: ['Força', 'Constituição', 'Destreza'], must: 'Aparência' }
+            when 60..69
+                @change = [:str, :con, :dex]
+                @must = :app
+                @points = { change: 15, must: 15 }
+                @details = { change: ['Força', 'Constituição', 'Destreza'], must: 'Aparência' }
+            when 70..79
+                @change = [:str, :con, :dex]
+                @must = :app
+                @points = { change: 20, must: 20 }
+                @details = { change: ['Força', 'Constituição', 'Destreza'], must: 'Aparência' }
+            when 80..89
+                @change = [:str, :con, :dex]
+                @must = :app
+                @points = { change: 25, must: 25 }
+                @details = { change: ['Força', 'Constituição', 'Destreza'], must: 'Aparência' }
+            else
+                raise ActionController::RoutingError.new('Invalid age') #should never run
+            end
         end
     end
 
